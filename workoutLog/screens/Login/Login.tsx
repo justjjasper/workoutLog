be566663b1582b520ad906efe790e6c1acc94792
JASPER <jasjasper101@gmail.com>
2023-07-20 18:21:07 -0700

    Implement Sign Up text and Login Form Container

--- workoutLog/screens/Login/Login.tsx
+++ workoutLog/screens/Login/Login.tsx
@@ -4,20 +4,38 @@ import { useState } from 'react';
 export default function Login () {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi Me</Text>
+      <View style={styles.signUpContainer}>
+      <Text style={styles.signUpText}>Sign Up</Text>
+      </View>
+      <View style={styles.form}>
+
+      </View>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#E4E5E3',
     justifyContent: 'center',
-    alignContent: 'center',
-    flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  text: {
-    fontSize: 50
+  signUpText: {
+    fontSize: 24,
+    textDecorationLine: 'underline'
+  },
+  signUpContainer: {
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    bottom: 180,
+    paddingRight: 10
+  },
+  form : {
+    height: '45%',
+    width: '90%',
+    borderWidth: 1,
+    backgroundColor: '#FEFEFE',
+    borderRadius: 5
   }
 })
