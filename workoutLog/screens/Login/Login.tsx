d8906e127e2d58ed0b6c9e46e7b29a74d2788158
JASPER <jasjasper101@gmail.com>
2023-07-21 16:06:03 -0700

    Implement skeleton features of Login Page without functionality

--- workoutLog/screens/Login/Login.tsx
+++ workoutLog/screens/Login/Login.tsx
@@ -1,15 +1,52 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 
 export default function Login () {
+  const [show, setShow] = useState<boolean>(true);
+
   return (
     <View style={styles.container}>
       <View style={styles.signUpContainer}>
-      <Text style={styles.signUpText}>Sign Up</Text>
+        <Text style={styles.signUpText}>Sign Up</Text>
       </View>
-      <View style={styles.form}>
 
+      <View style={styles.form}>
+      <View style={styles.emailContainer}>
+        <Text style={styles.text}>Email Address</Text>
+        <TextInput
+          style={styles.textInput}
+        />
       </View>
+
+      <View style={styles.passwordContainer}>
+        <Text style={styles.text}>Password</Text>
+        <TextInput
+          style={styles.textInput}
+          secureTextEntry= {show}
+        />
+        <TouchableOpacity
+          style={styles.showTextContainer}
+          onPress={() => setShow(!show)}
+        >
+          <Text style={styles.showText}>Show</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.loginContainer}>
+        <TouchableOpacity
+          style={styles.loginButton}
+        >
+          <Text style={styles.loginText}>Log In</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableOpacity>
+          <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+
     </View>
   )
 };
@@ -19,23 +56,74 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E4E5E3',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   signUpText: {
     fontSize: 24,
-    textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
+    color: '#77C7E8'
   },
   signUpContainer: {
     alignSelf: 'flex-end',
     alignItems: 'center',
-    bottom: 180,
-    paddingRight: 10
+    bottom: 175,
+    paddingRight: 15
   },
   form : {
     height: '45%',
     width: '90%',
-    borderWidth: 1,
     backgroundColor: '#FEFEFE',
-    borderRadius: 5
+    borderRadius: 5,
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+  },
+  text: {
+    color: '#565758',
+    fontSize: 18,
+    paddingBottom: 25
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    fontSize: 18
+  },
+  emailContainer: {
+    width: '80%',
+  },
+  passwordContainer: {
+    width: '80%',
+    bottom: 9
+  },
+  showTextContainer: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    top: 40
+  },
+  showText: {
+    alignSelf: 'flex-end',
+    textDecorationLine: 'underline',
+    fontSize: 18,
+    color: '#565758'
+  },
+  loginButton: {
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    backgroundColor: '#AFDBE1',
+    borderRadius: 25,
+    alignSelf: 'center'
+  },
+  loginContainer: {
+    width: '80%'
+  },
+  loginText: {
+    fontSize: 20,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  forgotPasswordText: {
+    textDecorationLine: 'underline',
+    fontSize: 18,
+    color: '#77C7E8'
   }
+
 })
