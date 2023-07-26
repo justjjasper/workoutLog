269394d6c118b6bd1c34f7ce6da189017bdec298
JASPER <jasjasper101@gmail.com>
2023-07-25 17:55:02 -0700

    Implement log out button in Profile Stack Screen

--- workoutLog/screens/Profile/Profile.tsx
+++ workoutLog/screens/Profile/Profile.tsx
@@ -1,9 +1,42 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
+import { loginEmailAddress } from '../../actions';
 
 export default function Profile() {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+
+
+  const renderHeaderRight = () => {
+    return (
+      <View style={styles.logOutContainer}>
+        <Button
+          title= 'Log Out'
+          onPress={() => dispatch(loginEmailAddress(''))}
+          color= 'white'
+          />
+      </View>
+    )
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: renderHeaderRight
+    })
+  });
+
   return (
     <View>
       <Text>I'm the profile screen hehe</Text>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  logOutContainer: {
+    paddingRight: 10
+  }
+})
