5e72ec64277c53e5d9ec9b251346ab1da66f0208
JASPER <jasjasper101@gmail.com>
2023-08-15 17:11:50 -0700

    Implement calendar and profile vector icons

--- workoutLog/screens/BottomTabNav.tsx
+++ workoutLog/screens/BottomTabNav.tsx
@@ -1,19 +1,30 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Home';
 import ProfileStackNavigator from './Profile/ProfileStackNavigator';
+import Icon from 'react-native-vector-icons/Ionicons'
+
 const Bottom = createBottomTabNavigator();
 
 export default function BottomTabNav() {
   return (
     <Bottom.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+
+          if (route.name === 'Calendar') {
+            return <Icon name='calendar-sharp' size={25} color={color} />;
+          } else if (route.name === 'Profile') {
+            return <Icon name='person-sharp' size={25} color={color} />;
+          }
+        },
         tabBarActiveTintColor: '#77C7E9',
-        headerStyle: {backgroundColor: '#77C7E9'},
-        headerTitleStyle: {color: 'white'}
-      }}
+        tabBarInactiveTintColor: 'gray',
+        headerStyle: { backgroundColor: '#77C7E9' },
+        headerTitleStyle: { color: 'white' },
+      })}
       >
-      <Bottom.Screen name='Home' component={Home} options={{ headerShown: false }}/>
-      <Bottom.Screen name='Profile Stack' component={ProfileStackNavigator} options={{ headerShown: false }}/>
+      <Bottom.Screen name='Calendar' component={Home} options={{ headerShown: false }}/>
+      <Bottom.Screen name='Profile' component={ProfileStackNavigator} options={{ headerShown: false }}/>
     </Bottom.Navigator>
   )
 };
